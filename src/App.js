e48dafed4a1c858886cfd31f671f50a4215eaf36
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2022-12-05 17:43:16 +0530

    Seperate all words from a paragraph.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 
 const handleClick = async () => {
+  // Fetch the data from url and store the text result in a variable
   const url = "https://www.terriblytinytales.com/test.txt";
   var textResponse;
   await fetch(url)
@@ -10,7 +11,11 @@ const handleClick = async () => {
     })
     .catch((error) => console.log(error));
 
-  console.log(textResponse);
+  // The textResponse is a paragraph which contain words
+  // Each words are seperated by spaces 
+  var words = [];
+  words = textResponse.replace(/[^A-Za-z0-9]+/g, " ").split(" ");
+  console.log(words);
 }
 
 function App() {
