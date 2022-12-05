208f1d09b12418a6f60bee784f3c5a2495f0b5d0
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2022-12-05 17:38:17 +0530

    Fetch the data from the url.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import "./App.css";
 
+const handleClick = async () => {
+  const url = "https://www.terriblytinytales.com/test.txt";
+  var textResponse;
+  await fetch(url)
+    .then(response => response.text())
+    .then((response) => {
+      textResponse = response;
+    })
+    .catch((error) => console.log(error));
+
+  console.log(textResponse);
+}
+
 function App() {
   return (
     <div className="App">
       <div className="button-container">
-        <button className="button">
+        <button className="button" onClick={handleClick}>
           Submit
         </button>
       </div>
